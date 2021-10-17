a5d0709d89798bd1e25515c5b60270be120b7479
Harsha Kondreddi <harsha.kondreddi@rksv.in>
2021-10-17 19:05:53 +0530

    "unused import removed"

--- kiteconnect/src/com/zerodhatech/godavari/TimeUtil.java
+++ kiteconnect/src/com/zerodhatech/godavari/TimeUtil.java
@@ -2,7 +2,6 @@ package com.zerodhatech.godavari;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class TimeUtil {
     public static String getWeekDayName() {
